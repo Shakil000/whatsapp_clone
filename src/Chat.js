48963a2b73bc2ff01cbaf1081c82b3ba28beeb7e
Shakil000 <racer.shakil111@gmail.com>
2020-12-07 20:23:27 +0600

    push

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,27 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, Search } from "@material-ui/icons";
+import { AttachFile, MoreVert, Search, SpaceBarOutlined } from "@material-ui/icons";
 import React, {useState, useEffect} from 'react';
+import InsertEmoticon from "@material-ui/icons/InsertEmoticon";
+import Mic from "@material-ui/icons/Mic";
 import "./Chat.css";
 
 function Chat() {
-
+    const [input, setInput] = useState("");
     const [seed,setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
     }, []);
+
+
+
+ const sendMessage = (e) =>{
+    e.preventDefault();
+    console.log("You typed >>>",input);
+
+    setInput("");
+ };
+
 
     return <div className="chat">
         <div className="chat__header">
@@ -31,10 +43,21 @@ function Chat() {
          </div>
         </div>
         <div className="chat__body">
-
+        <p className={`chat__message ${true &&"chat__reciever"}`}>
+        <span className="chat__name">shakil hasan</span> 
+        Hello
+        <span className="chat__timestamp">
+            5:57pm
+        </span>
+        </p>
         </div>
         <div className="chat__footer">
-
+      <InsertEmoticon/>
+      <form>
+          <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a Message" type="text"/>  
+          <button onClick={sendMessage} type="submit">Send a message</button>
+      </form>
+      <Mic/>
         </div>
     </div>;
     
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyA1pmX2W_MyL3wyEWVTp_zrrRS0EknJNuE",
     authDomain: "whatsapp-d121c.firebaseapp.com",
+
     projectId: "whatsapp-d121c",
     storageBucket: "whatsapp-d121c.appspot.com",
     messagingSenderId: "1070079638714",
     appId: "1:1070079638714:web:f9e1644baf5544d4b2d683",
     measurementId: "G-KGX1ZN3002"
   };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
